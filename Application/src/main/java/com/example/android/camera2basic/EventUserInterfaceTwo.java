a57c6908d70e9b569616518579ef37aa66e4223d
Stephen Gutknecht <stephen.gutknecht@gmail.com>
2016-06-15 14:19:50 -0400

    Event class

---
+++ Application/src/main/java/com/example/android/camera2basic/EventUserInterfaceTwo.java
@@ -0,0 +1,30 @@
+package com.example.android.camera2basic;
+
+/**
+ * Created by adminsag on 5/25/16.
+ * (c) Copyright 2016 Stephen A Gutknecht
+ * Dual license: 1. Closed-source proprietary by Stephen A. Gutknecht. All Rights Reserved. 2. Apache License, Version 2.0, on this specific file.
+ *
+ */
+public class EventUserInterfaceTwo {
+
+    public static final int CAMERA_ERROR_SPOTA = 1;
+    public static final int CAMERA_CAMERA2_API_DIALOG0 = 2;
+    public static final int CAMERA_PERMISSION_NEEDED0 = 3;
+    public static final int CAMERA_CONFIGURE_FAILED0 = 4;
+    public static final int CAPTURE_IMAGE_SAVED0 = 5;
+
+    public EventUserInterfaceTwo(int inEventCode)
+    {
+        eventCode = inEventCode;
+    }
+
+    public EventUserInterfaceTwo(int inEventCode, String stringPayload)
+    {
+        eventCode = inEventCode;
+        eventPayload0 = stringPayload;
+    }
+
+    public int eventCode = 0;
+    public String eventPayload0 = "";
+}
